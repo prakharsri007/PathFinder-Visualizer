d0b05d845da6d1adb51a98eeeab054eb4e67456a
shreya1881 <2704shreya@gmail.com>
2024-01-22 14:07:01 +0530

    symbols added

--- my-app/src/PathfindingVisualizer/Node/Node.jsx
+++ my-app/src/PathfindingVisualizer/Node/Node.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { FaArrowRight, FaCircle } from 'react-icons/fa';
+
 
 import './Node.css';
 
@@ -22,13 +24,17 @@ export default class Node extends Component {
       && 'node-wall') ||
        'node-normal';
 
+        const symbol = isStart ? <FaArrowRight className='start-symbol'/> : isFinish ? <FaCircle className='finish-symbol'/> : null;
+
     return (
       <div
         id={`node-${row}-${col}`}
         className={`node ${extraClassName}`}
         onMouseDown={() => onMouseDown(row, col)}
         onMouseEnter={() => onMouseEnter(row, col)}
-        onMouseUp={() => onMouseUp()}></div>
+        onMouseUp={() => onMouseUp()}>
+          {symbol}
+        </div>
     );
   }
 }
